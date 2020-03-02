505d9086c70998b34590101216071281e30f2cae
Kuddleman <edl1155@gmail.com>
2020-03-02 14:29:10 -0800

    add code for links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import '../App.css'
+import { Link, withRouter } from 'react-router-dom'
 
 class Navbar extends React.Component {
+   
 
   render() {
+
+    const navStyle = {
+      color: 'red'
+    }
     return (
-      <div>
-        Hello from Navbar
-      </div>
+      <nav>
+        <h3>Logo</h3>
+        <ul className="nav-links">
+        <Link style={navStyle} to='/'>
+          <li>Login</li>
+        </Link>
+        <Link style={navStyle} to='/profile'>  
+          <li>Profile</li>
+        </Link>
+        <Link style={navStyle} to='/music_page'> 
+          <li>Write Music!</li>
+        </Link>   
+        </ul>
+      </nav>
     )
   }
 }
