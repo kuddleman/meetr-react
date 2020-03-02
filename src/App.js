b1ae82a16ad4635f86dffe8ce6c956b547b42f86
Kuddleman <edl1155@gmail.com>
2020-03-02 14:12:39 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Login from './containers/Login'
 import Profile from './containers/Profile'
 import MusicPage from './containers/MusicPage'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Navbar />
-      <Login />
-      <Profile />
-      <MusicPage />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello from App</h1>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/music_page" component={MusicPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '../App.css'
 
 class Navbar extends React.Component {
 
