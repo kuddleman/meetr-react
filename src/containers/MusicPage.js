ca59d09b5e20cee8d9c6407b5bb286af5fa41270
Kuddleman <edl1155@gmail.com>
2020-03-03 15:50:19 -0800

    add MusicScore component

--- src/containers/MusicPage.js
+++ src/containers/MusicPage.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Typography from '@material-ui/core/Typography';
+// import Container from '@material-ui/core/Container';
+import ScoreComments from '../components/ScoreComments'
+// import { Column } from 'mui-flex-layout'
+// import { makeStyles } from '@material-ui/core/styles';
+import MusicScore from './MusicScore'
 
 class MusicPage extends React.Component {
 
   render() {
     return (
-      <div>
-        Hello from Music Page Container
-      </div>
+      <div className="music-score-container">
+        <div className="comments-container">
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+          <ScoreComments />
+        </div>
+        <div>
+           <MusicScore />
+        </div>
+      </div>   
     )
   }
 }
---
+++ src/containers/MusicScore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MusicScore = () => {
+  
+  return (
+    <div>
+      Hello from MusicScore 
+    </div>
+  )
+}
+
+export default MusicScore;
