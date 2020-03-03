223120a833d3a4b68c2991b3479e8e2d416f478d
monkeypineapple <davidrosenholz@gmail.com>
2020-03-03 15:40:35 -0800

    Add API interface to MusicPage

--- src/containers/MusicPage.js
+++ src/containers/MusicPage.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import Embed from 'flat-embed'
 
 class MusicPage extends React.Component {
 
-  render() {
+  componentDidMount(){
+      let container = document.getElementById("embed-container");
+      let embed = new Embed(container, {
+            "width": "1000",
+            "height": "1000",
+            "score": "5e5d4756c36a982b33b89373",
+            "embedParams": {
+                "mode": "edit",
+                "controlsPosition": "top",
+                "branding": false,
+                "appId": "5e5d8d05f9a7d640446f88b4"
+            }})
+
+  }
+render() {
+   
+
+  
     return (
       <div>
-        Hello from Music Page Container
+        <div id="embed-container"></div>
       </div>
     )
   }
